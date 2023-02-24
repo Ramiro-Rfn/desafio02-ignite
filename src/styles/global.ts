c5e7570bf99797bdb979d4608488f767c8ada08b
Ramiro Nzau <ramirofrancisco962014@gmail.com>
2023-02-24 10:11:47 +0100

    config external fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -24,4 +24,9 @@ export const GlobalStyle = createGlobalStyle`
     body, input, textarea, button {
         font-family: 'Roboto', sans-serif;
     }
+
+    h1,h2, h3,h4, h5, h6 {
+        font-family: 'Baloo 2', sans-serif;
+        line-height: 1.3;
+    }
 `
