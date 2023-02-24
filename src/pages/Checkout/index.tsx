af4ae15a418de81bb8e46b2dc1faa3181c676d3c
Ramiro Nzau <ramirofrancisco962014@gmail.com>
2023-02-24 09:53:31 +0100

    config routes

---
+++ src/pages/Checkout/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export default function Checkout(){
+    return (
+        <Container>
+            <h1>Checkout Page</h1>
+        </Container>
+    )
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export default function Home() {
+    return (
+        <Container>
+            <h1>Home page</h1>
+        </Container>
+    )
+}
